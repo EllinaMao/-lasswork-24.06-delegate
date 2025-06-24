ff5c42f8764ae133a34cfee75598d18e9e4fede7
Olga <xotaoshi@gmail.com>
2025-06-24 20:57:01 +0300

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task3
+{
+    /*
+     Задание 3
+Создайте приложения для выполнения арифметических операций. Поддерживаемые операции:
+■ Проверка числа на чётность;
+■ Проверка числа на нечётность;
+Практическое задание
+1
+■ Проверка является ли число простым;
+■ Проверка является ли число числом Фибоначчи.
+Обязательно используйте делегат типа Predicate.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 15;
+
+            Arifmetic.IsNum(num, Arifmetic.IsEven, "Чётность");
+            Arifmetic.IsNum(num, Arifmetic.IsOdd, "Нечётность");
+            Arifmetic.IsNum(num, Arifmetic.IsPrime, "Простое число");
+            Arifmetic.IsNum(num, Arifmetic.IsFibonacci, "Число Фибоначчи");
+            
+
+
+        }
+    }
+}
---
+++ Сlasswork 24.06 delegate/String show.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сlasswork_24._06_delegate
+{/*     Задание 1
+Создайте приложение, отображающее текстовое
+сообщение. Используйте механизм делегатов. Делегат
+должен возвращать void и принимать один параметр
+типа string (текст сообщения). Для тестирования приложения создайте различные методы для вызова через
+делегата.*/
+    internal static class StringShow
+    {
+        public static void Print (string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void PrintUpper(string message)
+        {
+            Console.WriteLine(message.ToUpper());
+        }
+        public static void PrintLower(string message)
+        {
+            Console.WriteLine(message.ToLower());
+        }
+
+        public static void PrintCat(string message)
+        {
+            message += " Cat";
+            Console.WriteLine(message);
+        }
+
+
+
+    }
+}
